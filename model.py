b20a8ff8e413967bdcc4351c2559310bfb6392d1
Kishore Bodke <b.kishore22@gmail.com>
2017-11-07 22:39:23 -0800

    Initial commit for P3
    Behaviour cloning project 3 initial tested
    
    Signed-off-by: Kishore Bodke <b.kishore22@gmail.com>

---
+++ model.py
@@ -0,0 +1,70 @@
+import csv
+import cv2
+import numpy as np
+
+import keras
+from keras.models import Sequential
+#from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D, Cropping2D
+#from keras.layers import Convolution2D
+#from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.pooling import MaxPooling2D
+
+lines = []
+
+with open('../training_data/driving_log.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	for line in reader:
+		lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+	for i in range(3):
+		source_path = line[i]
+		filename = source_path.split('/')[-1]
+		current_path = '../training_data/IMG/' + filename 
+		image = cv2.imread(current_path)
+		images.append(image)
+	correction = 0.2
+	measurement = float(line[3])
+	measurements.append(measurement)
+	measurements.append(measurement+correction)
+	measurements.append(measurement-correction)
+print(len(images))
+print(len(measurements))
+#exit()
+augmented_images = []
+augmented_measurements = []
+
+for image, measurement in zip(images, measurements):
+	augmented_images.append(image)
+	augmented_measurements.append(measurement)
+	augmented_images.append(cv2.flip(image,1))
+	augmented_measurements.append(measurement * -1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+#print(X_train.shape)
+#print(y_train.shape)
+#exit()
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mse')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+model.save('model.h5')
